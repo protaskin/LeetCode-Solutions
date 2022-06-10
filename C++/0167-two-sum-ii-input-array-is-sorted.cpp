6eefc9280197e37c5678c1362c0944c7ccda3cd3
Artyom Protaskin <a.protaskin@gmail.com>
2022-06-10 18:54:10 +0300

    Intend code using only spaces ಠ_ಠ

--- C++/0167-two-sum-ii-input-array-is-sorted.cpp
+++ C++/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(const vector<int>& numbers, const int target) {
-    	int l = 0;
-    	int r = numbers.size() - 1;
+        int l = 0;
+        int r = numbers.size() - 1;
 
-    	while (true) {
-    		const int sum = numbers[l] + numbers[r];
-    		if (sum > target) {
-    			--r;
-    		} else if (sum < target) {
-    			++l;
-    		} else {
-    			return {l + 1, r + 1}; // 1-indexed
-    		}
-    	}
+        while (true) {
+            const int sum = numbers[l] + numbers[r];
+            if (sum > target) {
+                --r;
+            } else if (sum < target) {
+                ++l;
+            } else {
+                return {l + 1, r + 1}; // 1-indexed
+            }
+        }
     }
 };
